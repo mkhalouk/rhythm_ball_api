4840d1fda1ef204635172124fcf164f42b9eb31f
mkhalouk <musta.khalouk@gmail.com>
2023-04-13 11:24:32 +0200

    fix: cors

--- src/api.js
+++ src/api.js
@@ -11,6 +11,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use(express.urlencoded())
 // app.use(express.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    next()
+});
 app.use(cors());
 const uri = 'mongodb+srv://rhythmgame:rhythmgame@rhythmgame.5b3ushb.mongodb.net/rhythmgamedb?retryWrites=true&w=majority'
 
